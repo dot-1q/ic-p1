6529f00666437483c1b26c0a086ea564d76188e9
tiagoadonis <tiago.adonis@ua.pt>
2021-11-17 18:37:14 +0000

    Ex5 finished

---
+++ 5/entropy.py
@@ -0,0 +1,34 @@
+import sys
+import math
+
+def main():
+    entropy()
+
+def entropy():
+    print('Opening file {}...'.format(sys.argv[1]))
+    with open(sys.argv[1], 'rb') as f:
+        byteArr = list(f.read())
+    fileSize = len(byteArr)
+    print
+    print('File size in bytes: {:,d}'.format(fileSize))
+    # calculate the frequency of each byte value in the file
+    print('Calculating Shannon entropy of file. Please wait...')
+    freqList = []
+    for b in range(256):
+        ctr = 0
+        for byte in byteArr:
+            if byte == b:
+                ctr += 1
+        freqList.append(float(ctr) / fileSize)
+    # Shannon entropy
+    ent = 0.0
+    for freq in freqList:
+        if freq > 0:
+            ent = ent + freq * math.log(freq, 2)
+    ent = -ent
+    print('Shannon entropy: {}'.format(ent))
+    print
+
+
+if __name__== "__main__":
+    main()
--- 5/graph.py
+++ 5/graph.py
@@ -1,21 +1,50 @@
 import matplotlib.pyplot as plt
-import numpy as np
+import math
 
-# Abre o ficheiro com os dados
-f = open("data.txt", "r")
+def main():
+    # Abre o ficheiro com os dados
+    f = open("data.txt", "r")
 
-# Armazena todos os chars numa lista
-chars = []
-for line in f:
-    line.split("\n")
-    chars += line[0]
+    # Calcula a entropia do ficheiro
+    entropy("data.txt")
 
-# Desenha o histograma com a frequência de cada char
-plt.figure(figsize=(20,10))
-# rwidth=0.5 -> espaço entre cada barra
-# bins=68 -> número de barras; existem 68 caracteres diferentes
-plt.hist(chars, rwidth=0.5, bins=68)
-plt.xlabel('Characters')
-plt.ylabel('Frequency')
-plt.title('Characters Frequency Histogram')
-plt.show()
+    # Armazena todos os chars numa lista
+    chars = []
+    for line in f:
+        line.split("\n")
+        chars += line[0]
+
+    print("Showing graph...")
+    # Desenha o histograma com a frequência de cada char
+    plt.figure(figsize=(20,10))
+    # rwidth=0.5 -> espaço entre cada barra
+    # bins=68 -> número de barras; existem 68 caracteres diferentes
+    plt.hist(chars, rwidth=0.5, bins=68)
+    plt.xlabel('Characters')
+    plt.ylabel('Frequency')
+    plt.title('Characters Frequency Histogram')
+    plt.show()
+
+def entropy(filename):
+    with open(filename, 'rb') as f:
+        byteArr = list(f.read())
+    fileSize = len(byteArr)
+    
+    freqList = []
+    for b in range(256):
+        ctr = 0
+        for byte in byteArr:
+            if byte == b:
+                ctr += 1
+        freqList.append(float(ctr) / fileSize)
+    
+    # Shannon entropy
+    ent = 0.0
+    for freq in freqList:
+        if freq > 0:
+            ent = ent + freq * math.log(freq, 2)
+    ent = -ent
+    print('Shannon entropy: '+str(ent))
+
+if __name__== "__main__":
+    main()
